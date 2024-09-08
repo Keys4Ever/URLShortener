d6129878124ee002bff80e5fb63eba50ab2f7e8b
Keys4Ever <cubillagonzalo@outlook.com>
2024-09-07 23:06:14 -0300

    Reverted changes.

--- app.js
+++ app.js
@@ -1,46 +1,30 @@
 import { createClient } from "@libsql/client";
-import cors from "cors";
-import { nanoid } from "nanoid";
+import cors from 'cors';
+import { nanoid } from 'nanoid';
 import express from "express";
-import dotenv from "dotenv";
-import { auth } from "express-openid-connect";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 
-// Configuración de autenticación con Auth0
-const authConfig = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.AUTH0_SECRET, // Debe estar almacenado en una variable de entorno
-  baseURL: "http://keys.lat",
-  clientID: "E5baYMNpD1YDHuKiXh2A6yAYw58bGiiA",
-  issuerBaseURL: "https://dev-b08mg8ad2fe7mbdr.us.auth0.com",
-};
-
-// Middleware de autenticación
-app.use(auth(authConfig));
-
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Conectar con la base de datos Turso
+// Connect to the Turso database
 export const turso = createClient({
   url: process.env.TURSO_DATABASE_URL,
   authToken: process.env.TURSO_DATABASE_TOKEN,
 });
 
-// Buscar una URL acortada
+// Look for a shortened URL.
 async function lookForUrl(id) {
   const response = await turso.execute({
     sql: "SELECT original_url FROM shortened_urls WHERE id = (:_id)",
-    args: { _id: id },
+    args: {_id: id},
   });
   if (response.rows.length === 0) {
     throw new Error("404. URL not found.");
@@ -48,33 +32,30 @@ async function lookForUrl(id) {
   return response.rows[0][0];
 }
 
-// Servir archivos estáticos desde la carpeta public
+// Serve static files from the public folder.
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-async function alreadyExists(thing) {
+async function alreadyExists(thing){
   const result = await turso.execute({
     sql: "SELECT original_url FROM shortened_urls WHERE id = (:_id)",
-    args: { _id: thing },
+    args: {_id: thing},
   });
   return result.rowsAffected > 0;
 }
 
-// Endpoint para crear una nueva URL acortada
-app.post("/shortUrl", async (req, res) => {
+// Post method to create a new shortened URL.
+app.post('/shortUrl', async (req, res) => {
   try {
     let originalUrl = req.body.originalUrl;
     let wantedUrl = req.body.wantedUrl;
 
     if (!originalUrl) {
       return res.status(400).json({ error: "La URL original es requerida." });
-    } else if (
-      !originalUrl.startsWith("https://") &&
-      !originalUrl.startsWith("http://")
-    ) {
+    } else if (!originalUrl.startsWith('https://') && !originalUrl.startsWith('http://')) {
       originalUrl = `https://${originalUrl}`;
     }
 
@@ -86,11 +67,7 @@ app.post("/shortUrl", async (req, res) => {
       }
     } else {
       if (await alreadyExists(wantedUrl)) {
-        return res
-          .status(409)
-          .json({
-            error: "La URL solicitada ya existe. Por favor, intenta usar otra.",
-          });
+        return res.status(409).json({ error: "La URL solicitada ya existe. Por favor, intenta usar otra." });
       } else {
         id = wantedUrl;
       }
@@ -107,16 +84,8 @@ app.post("/shortUrl", async (req, res) => {
       res.status(500).json({ error: "No se pudo crear la URL acortada." });
     }
   } catch (error) {
-    if (
-      error.message.includes(
-        "SQLITE_CONSTRAINT: SQLite error: UNIQUE constraint failed: shortened_urls.id"
-      )
-    ) {
-      res
-        .status(409)
-        .json({
-          error: "La URL deseada ya está en uso. Por favor, intenta usar otra.",
-        });
+    if (error.message.includes("SQLITE_CONSTRAINT: SQLite error: UNIQUE constraint failed: shortened_urls.id")) {
+      res.status(409).json({ error: "La URL deseada ya está en uso. Por favor, intenta usar otra." });
     } else {
       console.error(error);
       res.status(500).json({ error: "Error interno del servidor." });
@@ -124,7 +93,7 @@ app.post("/shortUrl", async (req, res) => {
   }
 });
 
-// Redirigir a la URL original cuando se accede a una URL acortada
+// Redirect to the original URL when a shortened URL is accessed.
 app.get("/:shortenedUrl", async (req, res) => {
   try {
     const originalUrl = await lookForUrl(req.params.shortenedUrl);
@@ -134,7 +103,7 @@ app.get("/:shortenedUrl", async (req, res) => {
   }
 });
 
-// Endpoint de API para obtener la URL original con una URL acortada
+// API endpoint to get the original url with a shortened URL.
 app.get("/api/original-url/:shortenedUrl", async (req, res) => {
   try {
     const originalUrl = await lookForUrl(req.params.shortenedUrl);
@@ -143,11 +112,5 @@ app.get("/api/original-url/:shortenedUrl", async (req, res) => {
     res.status(error.status || 500).json({ error: error.message });
   }
 });
-
-// Ruta para verificar si el usuario está autenticado
-app.get("/", (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
-});
-
-// Exportar la aplicación Express para Vercel
+// Export the Express app for Vercel
 export default app;
